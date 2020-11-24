53b81c029bc7987493e9b1ec61d46651f5c3ed0f
lop19029 <alexhumbertolds@gmail.com>
2020-11-24 19:48:16 -0300

    Adding javadoc comments to MainActivity

--- app/src/main/java/com/example/projectmemory/MainActivity.java
+++ app/src/main/java/com/example/projectmemory/MainActivity.java
@@ -10,18 +10,32 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
-
+/**
+ * Set up the main page and lists of the app
+ *
+ * <p>
+ *     Controls list creation, and list classification.
+ *     It loads all the {@link com.example.projectmemory.ListContainer}, and let's the user display them.
+ *     Save lists containers onStop using {@link MainActivity#saveLists()} and SharedPreferences.
+ *     Load list containers onCreate using {@link MainActivity#loadLists()}.
+ * </p>
+ * */
 public class MainActivity extends AppCompatActivity {
-    ListContainer CommonLists;
+    ListContainer CommonLists; //Holds the common lists
     public static final String JSON_DATA = "JSON_DATA";
     public static final String APP_PREFS = "APPLICATION_PREFERENCES";
-
+    /**
+     * Create the main page and load the lists
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         loadLists();
     }
+    /**
+     * Save the lists using SharedPreferences
+     */
     @Override
     protected void onStop() {
         // call the superclass method first
@@ -30,6 +44,18 @@ public class MainActivity extends AppCompatActivity {
         //Update and save all current lists in SharedPreferences
         saveLists();
     }
+    /**
+     * When the user press the button, create a new common list
+     *
+     * <p>
+     *     Takes the user input from the EditText in {@link com.example.projectmemory.R.layout#activity_main}
+     *     and use it as a name to crate a new {@link com.example.projectmemory.List} and add it to
+     *     {@link com.example.projectmemory.MainActivity#CommonLists}
+     * </p>
+     *
+     * @param view
+     * @return {@link com.example.projectmemory.List}
+     * */
     public void createCommonList(View view){
         //Get list name
         EditText list = (EditText) findViewById(R.id.CommonListName);
@@ -38,7 +64,13 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this, "List added to your Common lists", Toast.LENGTH_SHORT).show();
 
     }
-
+    /**
+     * Save all the {@link com.example.projectmemory.ListContainer}
+     *
+     * <p>
+     *     Serialize all the {@link com.example.projectmemory.ListContainer} objects into JSON
+     *     objects and then use shared preferences to save them all.
+     * </p>*/
     public void saveLists(){
         //Use SharedPreferences to store all the lists
         SharedPreferences sharedPreferences = getSharedPreferences(APP_PREFS, Context.MODE_PRIVATE);
